d556f624c737529715277ecd938b5f641713995e
szeth <geoffreyoliaro@gmail.com>
2020-02-19 11:47:07 +0300

    initial commit

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:kenya_airports_authorities/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return either home or authenticate widget
+    return Home();
+  }
+}
